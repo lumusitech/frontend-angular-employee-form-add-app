115abec18f32d92170f7b1aac2f38293381be1e3
lumusitec <lumusitec@gmail.com>
2021-02-08 16:18:01 -0300

    simple revision

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { debounceTime } from 'rxjs/operators';
 import { EmployeeService } from '../employee.service';
 import { Employee } from '../models/employee.model';
 
