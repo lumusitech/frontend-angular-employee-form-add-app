d400df564d2d87c194d944feee6b304dad450a20
lumusitech <lumusitech@gmail.com>
2023-11-12 18:35:21 -0300

    feat: change types and update photos url

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,20 +2,73 @@ import { Injectable } from '@angular/core';
 import { Employee } from './models/employee.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
-
   employees: Employee[];
 
   constructor() {
     this.employees = [
-      { name: 'Carlos', lastname: 'Figueroa', email: 'lux@g.com', age: 40, department: 'systems', password: '1234', image: 'https://images.generated.photos/wYT5FEQJrls_4fBH_NRxauP0z08VzzMdsL7fx8HMY10/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2MDUzNDAuanBn.jpg' },
-      { name: 'Elías', lastname: 'Figueroa', email: 'eli@g.com', age: 40, department: 'systems', password: '1234', image: 'https://images.generated.photos/6Cow6kC0VAHG-XskwR8l6m4tbQS7V2bjVnzm957FJ2o/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4NDc5ODYuanBn.jpg' },
-      { name: 'Melanie', lastname: 'Figueroa', email: 'melu@g.com', age: 20, department: 'marketing', password: '1234', image: 'https://images.generated.photos/ndSjfDZwGC5bFGBVA1IdRMroeFEL3vraLs_mQEMJ5S8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAyMzYzNjAuanBn.jpg' },
-      { name: 'Emma', lastname: 'Ponce', email: 'emma@g.com', age: 42, department: 'Marketing', password: '1234', image: 'https://images.generated.photos/voTeDs61yu2axbT6BuiBrBMuUwY_Xb65VU5ZbKjtH5s/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA1ODY4MDYuanBn.jpg' },
-      { name: 'Zion', lastname: 'Figueroa', email: 'zion@g.com', age: 35, department: 'systems', password: '1234', image: 'https://images.generated.photos/AxUa1S3cOemOKrvvxXJUmLjZYKdu879xpmDM8aCXhlA/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2OTk5NjkuanBn.jpg' },
-      { name: 'Lucas', lastname: 'Marx', email: 'marx@g.com', age: 42, department: 'systems', password: '1234', image: 'https://images.generated.photos/lgRJN0Oyw-E9oBrhDfUj_CjWCXHrQIOEFZsJ3RibNf8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4NDcxMjMuanBn.jpg' },
+      {
+        name: 'Carlos',
+        lastname: 'Figueroa',
+        email: 'lux@g.com',
+        age: 40,
+        department: 'systems',
+        password: '1234',
+        image:
+          'https://images.generated.photos/YceTKZkXLQUO9n1uUPmF5sKzEd0acWltO9JNr7u_meE/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NDY1MzU3LmpwZw.jpg',
+      },
+      {
+        name: 'Elías',
+        lastname: 'Figueroa',
+        email: 'eli@g.com',
+        age: 40,
+        department: 'systems',
+        password: '1234',
+        image:
+          'https://images.generated.photos/SICvuOz0dlLhJLXyiXEnXmkIR5yOZD2YmLiOLrgbmLo/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTM5NDIzLmpwZw.jpg',
+      },
+      {
+        name: 'Melanie',
+        lastname: 'Figueroa',
+        email: 'melu@g.com',
+        age: 20,
+        department: 'marketing',
+        password: '1234',
+        image:
+          'https://images.generated.photos/0DhV9fBq2tNXblsn85Jofsx90JyL1ekZe1zcysncpOY/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjI2NjI0LmpwZw.jpg',
+      },
+      {
+        name: 'Emma',
+        lastname: 'Ponce',
+        email: 'emma@g.com',
+        age: 42,
+        department: 'Marketing',
+        password: '1234',
+        image:
+          'https://images.generated.photos/v_0FtdOTOY0OTPz1jlH52d9N_wNNC_c1A2171a0ra6o/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODE1MDQ5LmpwZw.jpg',
+      },
+      {
+        name: 'Zion',
+        lastname: 'Figueroa',
+        email: 'zion@g.com',
+        age: 35,
+        department: 'systems',
+        password: '1234',
+        image:
+          'https://images.generated.photos/J1yuFaFZkQTOLX5MqIo37wRp93DJGiQ06gPaDV3-ri0/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjAzNjEyLmpwZw.jpg',
+      },
+      {
+        name: 'Lucas',
+        lastname: 'Marx',
+        email: 'marx@g.com',
+        age: 42,
+        department: 'systems',
+        password: '1234',
+        image:
+          'https://images.generated.photos/7XmMCNIsXU305EKA5NcsoUhmd5QBfjAT1UgNU7CXnV4/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MDE1NTM5LmpwZw.jpg',
+      },
     ];
   }
 
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import {
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
 
 import { EmployeeService } from '../employee.service';
 import { Employee } from '../models/employee.model';
@@ -7,46 +13,35 @@ import { Employee } from '../models/employee.model';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.sass']
+  styleUrls: ['./form.component.sass'],
 })
 export class FormComponent implements OnInit {
-
   form: UntypedFormGroup;
 
   constructor(private employeeService: EmployeeService) {
     this.form = new UntypedFormGroup({
       name: new UntypedFormControl('', [
         Validators.required,
-        this.stringValidator
+        this.stringValidator,
       ]),
       lastname: new UntypedFormControl('', [
         Validators.required,
-        this.stringValidator
+        this.stringValidator,
       ]),
       email: new UntypedFormControl('', [
         Validators.required,
-        this.emailValidator
+        this.emailValidator,
       ]),
-      age: new UntypedFormControl('', [
-        Validators.required,
-        this.ageValidator
-      ]),
-      department: new UntypedFormControl('', [
-        Validators.required
-      ]),
-      password: new UntypedFormControl('', [
-        Validators.required
-      ]),
-      image: new UntypedFormControl('', [
-        Validators.required
-      ])
+      age: new UntypedFormControl('', [Validators.required, this.ageValidator]),
+      department: new UntypedFormControl('', [Validators.required]),
+      password: new UntypedFormControl('', [Validators.required]),
+      image: new UntypedFormControl('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
-    this.form.controls.age.valueChanges.subscribe(values =>
-      console.log(
-        /^[1-9]$|^[1-9][0-9]$|^1[0-2][0-9]$/.test(values))
+    this.form.controls.age.valueChanges.subscribe((values) =>
+      console.log(/^[1-9]$|^[1-9][0-9]$|^1[0-2][0-9]$/.test(values))
     );
   }
 
@@ -54,7 +49,13 @@ export class FormComponent implements OnInit {
     console.log(this.form.value);
     const $ = this.form.value;
     const employee: Employee = new Employee(
-      $.name, $.lastname, $.email, $.age, $.department, $.password, $.image
+      $.name,
+      $.lastname,
+      $.email,
+      $.age,
+      $.department,
+      $.password,
+      $.image
     );
 
     this.employeeService.insert(employee);
@@ -62,46 +63,37 @@ export class FormComponent implements OnInit {
     this.form = new UntypedFormGroup({
       name: new UntypedFormControl('', [
         Validators.required,
-        this.stringValidator
+        this.stringValidator,
       ]),
       lastname: new UntypedFormControl('', [
         Validators.required,
-        this.stringValidator
+        this.stringValidator,
       ]),
       email: new UntypedFormControl('', [
         Validators.required,
-        this.emailValidator
+        this.emailValidator,
       ]),
-      age: new UntypedFormControl('', [
-        Validators.required,
-        this.ageValidator
-      ]),
-      department: new UntypedFormControl('', [
-        Validators.required
-      ]),
-      password: new UntypedFormControl('', [
-        Validators.required
-      ]),
-      image: new UntypedFormControl('', [
-        Validators.required
-      ])
+      age: new UntypedFormControl('', [Validators.required, this.ageValidator]),
+      department: new UntypedFormControl('', [Validators.required]),
+      password: new UntypedFormControl('', [Validators.required]),
+      image: new UntypedFormControl('', [Validators.required]),
     });
   }
 
-  stringValidator(form: UntypedFormGroup): object | null {
-    let value = form.value;
+  stringValidator(control: AbstractControl): ValidationErrors | null {
+    let value = control.value;
     if (/^[a-zA-Z\s^/d]*$/.test(value)) return null;
     return { stringValidator: { error: 'it is allow only letters' } };
   }
 
-  emailValidator(form: UntypedFormGroup): object | null {
+  emailValidator(control: AbstractControl): ValidationErrors | null {
     //if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(form.value)) return null;
-    if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(form.value)) return null;
-    return { emailValidator: { error: "Formato de email incorrecto" } };
+    if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(control.value)) return null;
+    return { emailValidator: { error: 'Formato de email incorrecto' } };
   }
 
-  ageValidator(form: UntypedFormGroup): object | null {
-    if (/^[1-9]$|^[1-9][0-9]$|^1[0-2][0-9]$/.test(form.value)) return null;
+  ageValidator(control: AbstractControl): ValidationErrors | null {
+    if (/^[1-9]$|^[1-9][0-9]$|^1[0-2][0-9]$/.test(control.value)) return null;
     return { ageValidator: { error: 'invalid age format' } };
   }
 }
